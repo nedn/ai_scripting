fbab627ca53856e7f050ba0c3ea7c5af0e6af1b0
Nghia Nguyen <NghiaNguyenBH@gmail.com>
2025-04-05 21:43:36 -0600

    Update run_tests.py script to add test name filtering

--- run_tests.py
+++ run_tests.py
@@ -4,14 +4,31 @@ import unittest
 import sys
 import os
 
-def run_tests():
+def _extract_test_cases(test):
+    for t in test:
+        if isinstance(t, unittest.TestSuite):
+            yield from _extract_test_cases(t)
+        else:
+            yield t
+
+def run_tests(test_name=None):
     # Get the directory containing this script
     current_dir = os.path.dirname(os.path.abspath(__file__))
     
     # Discover all test files in the current directory and subdirectories
     loader = unittest.TestLoader()
     start_dir = current_dir
-    suite = loader.discover(start_dir, pattern='*_unittest.py', top_level_dir=current_dir)
+    
+    if test_name:
+        # If a test name is provided, only run tests that match the name
+        suite = unittest.TestSuite()
+        for test in loader.discover(start_dir, pattern='*_unittest.py', top_level_dir=current_dir):
+            for test_case in _extract_test_cases(test):
+                if test_name.lower() in str(test_case).lower():
+                    suite.addTest(test_case)
+    else:
+        # Run all tests if test name is provided
+        suite = loader.discover(start_dir, pattern='*_unittest.py', top_level_dir=current_dir)
     
     # Run the tests
     runner = unittest.TextTestRunner(verbosity=2)
@@ -21,4 +38,5 @@ def run_tests():
     return 0 if result.wasSuccessful() else 1
 
 if __name__ == '__main__':
-    sys.exit(run_tests())
+    test_name = sys.argv[1] if len(sys.argv) > 1 else None
+    sys.exit(run_tests(test_name))
